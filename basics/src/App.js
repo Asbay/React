4671cffd9698179b9541f4606a55c3bedd5de479
AsBay <asenabayhan@outlook.de>
2023-06-06 22:26:17 +0200

    06.06

---
+++ basics/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Apples, { Bananas1, Orange } from './components/01-my-house';
+import Jsx1 from './components/02-jsx/01-jsx';
+function App() {
+  return (
+    <>
+      <Apples />
+      <Bananas1 />
+      <Orange />
+      <Jsx1 />
+    </>
+  )
+}
+
+export default App
