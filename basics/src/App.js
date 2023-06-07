85f7fc0943bb5f99ff7b507c334f04ae045cdf8e
AsBay <asenabayhan@outlook.de>
2023-06-07 22:17:22 +0200

    07.06

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import Apples, { Bananas1, Orange } from './components/01-my-house';
 import Jsx1 from './components/02-jsx/01-jsx';
+import Jsx2 from './components/02-jsx/02-jsx';
+import Jsx3 from './components/02-jsx/03-jsx';
+import Jsx4 from './components/02-jsx/04-jsx';
+import InlineStyling from './components/03-styles.js/01-inline-styling';
+
+import ExternalStyling from './components/03-styles.js/03-external-styling';
+import ExternalModuleStyling from './components/03-styles.js/04-external-module-styling';
+import InternalStyling from './components/03-styles.js/02-internal-styling';
+
+
 function App() {
   return (
     <>
-      <Apples />
-      <Bananas1 />
-      <Orange />
-      <Jsx1 />
+        <Apples />
+            <br />
+            <Bananas1 />
+            <br />
+            <Orange />
+            <br />
+            <Jsx1 />
+            <br />
+            <Jsx2 />
+            <br />
+            <Jsx3 />
+            <br />
+            <Jsx4 />
+            <br />
+            <InlineStyling />
+            <br />
+            <InternalStyling />
+            <br />
+            <ExternalStyling />
+            <br />
+            <ExternalModuleStyling />
+            <br />
+
     </>
   )
 }
